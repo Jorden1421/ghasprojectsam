343a96719a546d9c8f181369a87481d8e2034598
Jorden McDonald <hey@jordenmcdonald.com>
2023-01-12 11:50:05 -0800

    codeql vulnerable test code

--- src/App.js
+++ src/App.js
@@ -2,34 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log("i got hit..");
-
-  console.log(
-    "environment variable is..",
-    process.env.REACT_APP_SENDGRID_API_KEY1
-  );
+  function createMarkup() {
+    return { __html: "<p>This information has my secret information</p>" };
+  }
 
   const secretApiKey = process.env.REACT_APP_SENDGRID_API_KEY1;
   const sendGridKey = process.env.REACT_APP_SENDGRID_API_KEY2;
 
   return (
-    <div className="App">
-      <p>Secret APi key is.. {secretApiKey}</p>
-      <p>Sendgrid key is.. {sendGridKey}</p>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root-app">
+      <div className="App">
+        <p>Secret APi key is.. {secretApiKey}</p>
+        <p>Sendgrid key is.. {sendGridKey}</p>
+        <div
+          style={{ border: "1px solid #000", margin: "20px", padding: "0px" }}
+          dangerouslySetInnerHTML={createMarkup()}
+          contentEditable="true"
+        />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
