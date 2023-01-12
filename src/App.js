dec24d784a83a7be8e4a36805d6d0c5bf35f3dca
Jorden McDonald <hey@jordenmcdonald.com>
2023-01-12 11:28:55 -0800

    exposed keys test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("i got hit..");
+
+  console.log(
+    "environment variable is..",
+    process.env.REACT_APP_SENDGRID_API_KEY1
+  );
+
+  const secretApiKey = process.env.REACT_APP_SENDGRID_API_KEY1;
+  const sendGridKey = process.env.REACT_APP_SENDGRID_API_KEY2;
+
   return (
     <div className="App">
+      <p>Secret APi key is.. {secretApiKey}</p>
+      <p>Sendgrid key is.. {sendGridKey}</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
